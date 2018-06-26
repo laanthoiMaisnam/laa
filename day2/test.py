7bf3ffdde9ffe87f08c0e0ec6d5594196b762386
laanthoi-pc <laanthoi@gmail.com>
2018-06-26 14:45:30 +0530

    1.0

--- day2/test.py
+++ day2/test.py
@@ -3,10 +3,11 @@ list_files= os.listdir("d:prank")
 print(list_files)
 os.chdir("d:prank")
 for file in list_files:
-    
+
     old_name=file
     print (old_name)
     new_name=old_name.lstrip("0123456789")
     print (new_name)
     os.rename(old_name,new_name)
+
 
